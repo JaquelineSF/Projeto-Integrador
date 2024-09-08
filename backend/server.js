70e096cf286a1714e294f0e7ef6d460c9278c685
JaqueSF <line.tick@hotmail.com>
2024-09-08 13:55:29 -0300

    Solucionando BUG do Footer da pagina HOME(index.com)

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
-const app = express();
+const { Pool } = require('pg');
+const cors = require('cors');
 
-// Definir rotas e middleware
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: 'SUPERUSER',
+    host: 'localhost',
+    database: 'ID_PET',
+    password: 'senha123',
+    port: 5432,
 });
 
-app.listen(3001, () => {
-  console.log('Servidor rodando na porta 3001');
+// Configuração do CORS
+app.use(cors());
+
+// Middleware para parsing de JSON
+app.use(express.json());
+
+// Exemplo de rota para testar a conexão com o banco de dados
+app.get('/data', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro ao consultar o banco de dados');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
